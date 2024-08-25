8c29d8dba0796dc8f8bd8bf2240c12d3b096ed68
José Cabarcas <jocabarcasg@hotmail.com>
2024-08-25 11:04:35 -0500

    feat: :sparkles: Link to character detail from list

--- containers/characters-page/recents-section/recent-item/index.tsx
+++ containers/characters-page/recents-section/recent-item/index.tsx
@@ -2,6 +2,7 @@
 
 import { CharacterRecent } from '@rick-and-morty-ch/types/character';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface RecentItemProps {
   character: CharacterRecent;
@@ -9,28 +10,30 @@ interface RecentItemProps {
 
 export const RecentItem = ({ character }: RecentItemProps) => {
   return (
-    <div
-      key={character.id}
-      className="w-48 flex-shrink-0 overflow-hidden rounded-lg bg-white shadow-md"
-    >
-      <div className="p-4">
-        <div className="bg-muted relative h-40 w-full overflow-hidden rounded-md">
-          <Image
-            src={character.image}
-            alt={character.name}
-            className="h-full w-full object-cover"
-            width={256}
-            height={150}
-          />
-          <div className="bg-muted text-muted-foreground absolute inset-0 flex items-center justify-center text-2xl font-bold opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
-            {character.name.slice(0, 2)}
+    <Link href={`/characters/${character.id}`} prefetch={false}>
+      <div
+        key={character.id}
+        className="w-48 flex-shrink-0 overflow-hidden rounded-lg bg-white shadow-md"
+      >
+        <div className="p-4">
+          <div className="bg-muted relative h-40 w-full overflow-hidden rounded-md">
+            <Image
+              src={character.image}
+              alt={character.name}
+              className="h-full w-full object-cover"
+              width={256}
+              height={150}
+            />
+            <div className="bg-muted text-muted-foreground absolute inset-0 flex items-center justify-center text-2xl font-bold opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+              {character.name.slice(0, 2)}
+            </div>
+          </div>
+          <div className="mt-4">
+            <p className="text-muted-foreground text-sm">#{character.id}</p>
+            <h3 className="truncate text-lg font-semibold">{character.name}</h3>
           </div>
         </div>
-        <div className="mt-4">
-          <p className="text-muted-foreground text-sm">#{character.id}</p>
-          <h3 className="truncate text-lg font-semibold">{character.name}</h3>
-        </div>
       </div>
-    </div>
+    </Link>
   );
 };
