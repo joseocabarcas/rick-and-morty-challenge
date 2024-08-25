317412e0b6e4f3e6b1450ea0fceefafc8285709d
José Cabarcas <jocabarcasg@hotmail.com>
2024-08-24 19:21:52 -0500

    fix: :bug: Header responsive

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,13 +4,16 @@ import '@rick-and-morty-ch/components/header/header.style.scss';
 export const Header = () => {
   return (
     <header className="header">
-      <Image
-        src="/rick_morty_header.webp"
-        alt="Logo Rick and Morty"
-        width={400}
-        height={200}
-        className="header__image"
-      />
+      <div className="header__image-container">
+        <Image
+          src="/rick_morty_header.webp"
+          alt="Logo Rick and Morty"
+          width={400}
+          height={200}
+          className="header__image"
+          priority
+        />
+      </div>
       <h1 className="header__title">Rick and Morty</h1>
     </header>
   );
